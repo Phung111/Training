61cf8db8c0c8f17c4e0e5037ced0bfda3448257a
Phung Acer <men01225423493@gmail.com>
2023-08-06 23:57:33 +0700

    training

--- feature/web/decox_design/mau_nha_dep.js
+++ feature/web/decox_design/mau_nha_dep.js
@@ -1,41 +1,83 @@
-const masonryRender = () => {
-    // let masonry_item = imgs.map(function (item) {
-    //     return `
-    //             <div class="masonry-item">
-    //                 <a href="#">
-    //                     <img src="${item.img}">
-    //                 </a>
-    //             </div>
-    //             `
-    // })
+var masonry_column_list = document.querySelectorAll('.masonry-col');
+var masonry_column_array = [...masonry_column_list];
 
-    var masonry_column_list = document.querySelectorAll('.masonry-col');
-    var masonry_column_array = [...masonry_column_list];
+let i = 1
+let j = 1
+let k = 1
+let height_min = 0;
+let height = 0;
+let height_object = {};
 
-    let i = 1
-    let j = 1
-    let k
-    let height_min = 0;
-    while (i <= masonry_column_array.length) {
 
-        if (j > masonry_column_array.length) {
-            for (var l = 1; l <= masonry_column_array.length; l++) {
-                let col_check = document.querySelector(`.masonry-col-${l}`);
-                let height = col_check.offsetHeight;
-                if (height_min < height) {
-                    height_min = height
-                    k = l
-                }
-            }
-            console.log('j :', j);
-            console.log('k :', k);
-            console.log('--------------------');
 
+const caculateColumnHieght = () => {
+    for (var l = 1; l <= masonry_column_array.length; l++) {
+        var col_check = document.querySelectorAll(`.masonry-item-${l}`);
+        console.log(col_check);
+        col_check.forEach(item => {
+            height += item.clientHeight;
+        });
+        console.log('height : ', height);
+        height_object['col_' + l] = height;
+        height = 0
+    }
+
+    height_min = height_object['col_' + 1]
+    k = 1
+    for (var l = 2; l <= masonry_column_array.length; l++) {
+        if (height_object['col_' + l] < height_min) {
+            height_min = height_object['col_' + l]
+            k = l
         }
+    }
 
+    console.log('k :', k);
+    console.log('----------------------------------------');
+
+}
+
+const getMinHeightColumn = (object) => {
+
+}
+
+var numberItem = 10
+var masonryRenderArray = []
+let min = 0
+let max = numberItem
+
+// const getMasonryArray = () => {
+//     console.log('min : ', min);
+//     console.log('max : ', max);
+//     masonryRenderArray = masonry.splice(min, max)
+//     console.log('masonryRenderArray: ', masonryRenderArray);
+// }
+
+
+
+// const showMore = () => {
+//     min += numberItem + 1
+//     max += numberItem
+//     getMasonryArray()
+//     masonryRender()
+// }
+
+const masonryRender = () => {
+
+    while (i <= masonry_column_array.length) {
         while (j <= masonry.length) {
+            console.log('j : ', j);
+            // console.log('pic_', j);
+            // caculateColumnHieght()
+            let column = i;
+            // if (j <= masonry_column_array.length) {
+            //     column = i
+            // } else {
+            //     column = k
+            // }
+
+
             let masonry_item = `
-                                    <div class="masonry-item relative flex-middle">
+                                    <div class="masonry-item masonry-item-${column} relative flex-middle">
                                         <a href="#">
                                             <img src="${masonry[j].img}">
                                         </a>
@@ -44,15 +86,8 @@ const masonryRender = () => {
                                         </div>
                                     </div>
                                  `
-            $(`.masonry-col-${i}`).append(masonry_item)
-            // if (j <= masonry_column_array.length) {
-            //     $(`.masonry-col-${i}`).append(masonry_item)
-            // } else {
-            //     $(`.masonry-col-${k}`).append(masonry_item)
-            // }
-
-            // $(`.masonry-col-${i}`).append(j)
-            if (j == all.length) {
+            $(`.masonry-col-${column}`).append(masonry_item)
+            if (j == masonry.length) {
                 return
             }
             i = i + 1;
@@ -65,6 +100,61 @@ const masonryRender = () => {
         }
 
     }
+    // while (i <= masonry_column_array.length) {
+
+    //     if (j > masonry_column_array.length) {
+
+
+    //     }
+
+    //     while (j <= masonry.length) {
+    //         let column;
+    //         if (j <= masonry_column_array.length) {
+    //             column = i
+    //         } else {
+    //             column = k
+    //         }
+
+
+    //         let masonry_item = `
+    //                                 <div class="masonry-item-${i} relative flex-middle">
+    //                                     <a href="#">
+    //                                         <img src="${masonry[j].img}">
+    //                                     </a>
+    //                                     <div class="absolute z-10 text-white fs-20 fw-700">
+    //                                         ${j}
+    //                                     </div>
+    //                                 </div>
+    //                              `
+    //         $(`.masonry-col-${i}`).append(masonry_item)
+    //         // if (j <= masonry_column_array.length) {
+    //         //     $(`.masonry-col-${i}`).append(masonry_item)
+    //         // } else {
+    //         //     $(`.masonry-col-${k}`).append(masonry_item)
+    //         // }
+
+    //         // $(`.masonry-col-${i}`).append(j)
+    //         if (j == all.length) {
+    //             return
+    //         }
+    //         i = i + 1;
+    //         j = j + 1;
+    //         break
+    //     }
+
+    //     if (i == (masonry_column_array.length + 1)) {
+    //         i = 1
+    //     }
+
+    // }
+
 }
 
+// getMasonryArray()
 masonryRender()
+
+
+// for (let i = 1; i <= masonry.length; i++) {
+//     masonryRenderArray = [...masonry[max]]
+//     console.log('masonryRenderArray : ', masonryRenderArray);
+// }
